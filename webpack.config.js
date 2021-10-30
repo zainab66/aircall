fb6c8a5f8caf8c90186b8ad4a759867acfe1d952
zainab66 <zainab66r@gmail.com>
2021-10-30 17:39:11 -0400

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,6 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: ['@babel/polyfill', './src/index.js'],
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  output: {
-    publicPath: "/"
-  },
-  devServer: {
-    historyApiFallback: true
-  },
   module: {
     rules: [
       {
@@ -42,3 +32,49 @@ module.exports = {
     })
   ]
 };
+
+
+// const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+// module.exports = {
+//   entry: ['@babel/polyfill', './src/index.js'],
+//   resolve: {
+//     extensions: ['.js', '.jsx']
+//   },
+//   output: {
+//     publicPath: "/"
+//   },
+//   devServer: {
+//     historyApiFallback: true
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.html$/,
+//         use: [
+//           {
+//             loader: 'html-loader',
+//             options: { minimize: true }
+//           }
+//         ]
+//       },
+//       {
+//         test: /\.css$/,
+//         use: [ 'style-loader', 'css-loader' ]
+//       },
+//       {
+//         test: /\.(js|jsx)$/,
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader'
+//         }
+//       }
+//     ]
+//   },
+//   plugins: [
+//     new HtmlWebPackPlugin({
+//       template: "./public/index.html",
+//       filename: "./index.html"
+//     })
+//   ]
+// };
